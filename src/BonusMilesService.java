6e13fd5f260d69c6d9ca8721bbd8d221a942543b
Irina-Bebenina <Irs.snoopy@gmail.com>
2020-08-05 11:56:20 +0300

    new file

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,9 +1,7 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        int price = 10_000;
         int bonus = 20;
-        int miles = price / bonus;
+        int miles = cost / bonus;
         return miles;
-
     }
 }
